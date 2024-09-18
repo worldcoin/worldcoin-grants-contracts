d6f75912f6813832ba1bdbd5659d9140be272bf8
Paul Schaaf <paul.schaaf@toolsforhumanity.com>
2024-09-18 16:36:11 +0200

    remove unused code in Grants4FirstBatch

--- src/Grants4FirstBatch/Grants4FirstBatch.sol
+++ src/Grants4FirstBatch/Grants4FirstBatch.sol
@@ -150,26 +150,6 @@ contract Grants4FirstBatch is Ownable2Step {
 }
 
 interface GnosisSafe {
-    /// @dev Allows a Module to execute a Safe transaction without any further confirmations.
-    /// @param to Destination address of module transaction.
-    /// @param value Ether value of module transaction.
-    /// @param data Data payload of module transaction.
-    /// @param operation Operation type of module transaction.
-    function execTransactionFromModule(
-        address to,
-        uint256 value,
-        bytes calldata data,
-        Enum.Operation operation
-    )
-        external
-        returns (bool success);
-}
-
-contract Enum {
-    enum Operation {
-        Call,
-        DelegateCall
-    }
 }
 
 interface IRecurringGrantDrop {
